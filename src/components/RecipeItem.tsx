2884608c01f1c9bed23c8d04d885a8dcb90e4863
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-11-13 01:44:52 +0500

    make changes

--- src/components/RecipeItem.tsx
+++ src/components/RecipeItem.tsx
@@ -6,28 +6,26 @@ interface RecipeItemProps {
 
 const RecipeItem = ({ item }: RecipeItemProps) => {
   return (
-    <div className="bg-white shadow-lg/30 w-1/3 h-140">
-      <img
-        className="h-52 w-full object-cover"
-        src={item.image}
-        alt=""
-      />
-      <h1>{item.name}</h1>
-      <h1>Ingredients:</h1>
-      <ul>
+    <div className="bg-white shadow-lg/30 w-[600px] h-160">
+      <img className="h-52 w-full object-cover" src={item.image} alt="" />
+      <h1 className="text-red-700 font-bold text-2xl mt-2">{item.name}</h1>
+      <h1 className="font-bold text-left pl-4">Ingredients:</h1>
+      <ul className="flex flex-wrap space-x-2 px-3 justify-center items-center">
         {item.ingredients.map((ingredient: string, index: number) => (
           <li key={index}>{ingredient}</li>
         ))}
       </ul>
 
-      <h1>Instructions:</h1>
-      <ul>
+      <h1 className="font-bold text-left pl-4">Instructions:</h1>
+      <ul className="flex flex-col flex-wrap px-3 justify-center items-center">
         {item.instructions.map((instruction: string, index: number) => (
           <li key={index}>{instruction}</li>
         ))}
       </ul>
 
-      <h1>Total Calories: 300</h1>
+      <h1 className="font-bold text-left pl-4">
+        Total Calories: {item.caloriesPerServing}
+      </h1>
     </div>
   );
 };
--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -1,31 +1,46 @@
-import { useState } from "react"
-import RecipeItem from "./RecipeItem"
-import type { RecipeType } from "../types/RecipeType"
-
+import { useEffect, useState } from "react";
+import RecipeItem from "./RecipeItem";
+import type { RecipeType } from "../types/RecipeType";
 
 const RecipeList = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [data, setData] = useState<RecipeType[]>([]);
+  const [errors, setErrors] = useState<string>("");
 
-    const [loading,setLoading] = useState<boolean>(false)
-    const [data,setData] = useState<RecipeType[]>([])
-    const [errors,setErrors] = useState<string>("")
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch("https://dummyjson.com/recipes");
+      if (!response.ok) {
+        throw new Error("There is a problem in fetching data");
+      }
+      const data = await response.json();
+      setData(data.recipes);
+    } catch (error) {
+      console.log("There is an error:", error);
+      setErrors((error as Error).message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div>
+      <h1>Recipe Book by Fahad Bin Javed</h1>
+      <div className="flex gap-4 flex-wrap">
         {loading && <p>Loading...</p>}
-        <h1>Recipe Booklet: Find Delecious Recipes</h1>
         {errors && <p>There is Error in WebPage</p>}
-        
-        {data.map((recipe)=>(
 
-        <RecipeItem 
-        key={data.id}
-        item={recipe}
-        />
+        {data.map((recipe) => (
+          <RecipeItem key={recipe.id} item={recipe} />
         ))}
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RecipeList
+export default RecipeList;
